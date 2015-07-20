b1a21e11409b16722ff10eb5d54a17bac1a6d93d
Abhiranjan Kumar <abhiranjan.kumar00@gmail.com>
2015-07-20 12:54:48 +0530

    Initializing basic setting files

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Filename: setup.sh
+
+ln -s -i -v ~/.vim/vimrc   ~/.vimrc
+ln -s -i -v ~/.vim/gvimrc  ~/.gvimrc
+ln -s -i -v ~/.vim/bashrc  ~/.bashrc
+ln -s -i -v ~/.vim/zshrc   ~/.zshrc
+ln -s -i -v ~/.vim/bash_aliases  ~/.bash_aliases
+# ln -s -i -v ~/.vim/gitconfig ~/.gitconfig
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,10 @@
+import XMonad
+import XMonad.Config.Gnome
+
+main = xmonad gnomeConfig
+       {
+         modMask     = mod4Mask
+       , borderWidth = 3
+       , terminal    = "terminator"
+       }
+
